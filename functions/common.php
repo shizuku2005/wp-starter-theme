e9a73eb156b89125a372a89913ab4abeca0a1f7b
kazuma-takeda <n0mu_n0li@icloud.com>
2023-08-16 20:54:27 +0900

    Contact Form 7の自動 pタグ無効

--- functions/common.php
+++ functions/common.php
@@ -69,3 +69,9 @@
 
     return str_replace(' src=', ' defer src=', $tag);
   }
+
+  // Contact Form 7の自動 pタグ無効
+  add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+  function wpcf7_autop_return_false() {
+    return false;
+  }
